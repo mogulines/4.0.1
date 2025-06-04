9b04601da9addcb0c2e3b009daf6515ec4abf1dd
mili <milibarraza18@gmail.com>
2025-06-04 03:58:36 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-
-
-
+// Asegúrate que funciones.js está cargado antes de este script
+document.addEventListener('DOMContentLoaded', function() {
+    const mostrarAsignacionesBtn = document.getElementById('mostrar-asignaciones-btn');
+    
+    mostrarAsignacionesBtn.addEventListener('click', mostrarAsignaciones);
+});
 
 
